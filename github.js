117aa78202a801bc98eea7a09a9f00c538dc8618
Ionică Bizău <bizauionica@yahoo.com>
2014-08-11 10:14:58 +0300

    Added splash screen

--- github.js
+++ github.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+// Set config
+global.CONFIG = {
+    root: __dirname
+}
 
+// Dependencies
+var SplashScreen = require("./lib/splash-screen");
+
+SplashScreen.show();
+setTimeout(function() {
+    //SplashScreen.hide();
+}, 1000);
