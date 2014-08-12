3da6f24354b51338747bf6f25356f3a51299a383
Ionică Bizău <bizauionica@yahoo.com>
2014-08-12 08:25:15 +0300

    Moved background box stuff in CONFIG

--- github.js
+++ github.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-var conf = require("./config");
+var conf = require("./config")
+  , Box = require("cli-box")
+  ;
 
 // Set config
 global.CONFIG = {
@@ -9,7 +11,22 @@ global.CONFIG = {
         w: process.stdout.columns || 212
       , h: process.stdout.rows || 56
     }
-}
+};
+CONFIG.background = new Box({
+    w: CONFIG.cli.w
+  , h: CONFIG.cli.h - 3
+  , marks: {
+        nw: "╔"
+      , n:  "═"
+      , ne: "╗"
+      , e:  "║"
+      , se: "╝"
+      , s:  "═"
+      , sw: "╚"
+      , w:  "║"
+      , b: " "
+    }
+});
 
 for (var key in conf) {
     CONFIG[key] = conf[key];
