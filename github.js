b4d5eb9c08cb4e8201eca210f173551c095c6ad3
Ionică Bizău <bizauionica@yahoo.com>
2014-08-12 12:06:37 +0300

    Integrate splash screen with login and news feeed

--- github.js
+++ github.js
@@ -2,7 +2,16 @@
 
 var conf = require("./config")
   , Box = require("cli-box")
+  , GitHub = new (require("github"))({
+        version: "3.0.0",
+        debug: true,
+        timeout: 5000
+    })
+  , Login = require("./lib/api/login")
   ;
+
+  debugger;
+console.log(process.env.NODE_DEBUG);
 
 // Set config
 global.CONFIG = {
@@ -11,6 +20,7 @@ global.CONFIG = {
         w: process.stdout.columns || 212
       , h: process.stdout.rows || 56
     }
+  , _github: GitHub
 };
 CONFIG.background = new Box({
     w: CONFIG.cli.w
@@ -37,8 +47,21 @@ var SplashScreen = require("./lib/ui/splash-screen")
   , MainStream = require("./lib/ui/stream")
   ;
 
-
-SplashScreen.show();
-setTimeout(function() {
-    MainStream();
-}, 1000);
+SplashScreen.show(function (err, output) {
+    if (err) { throw err; }
+    SplashScreen.updateMessage("Logging in ...");
+    Login(function (err, user) {
+        if (err) {
+            SplashScreen.updateMessage("Failed to login in: " + JSON.parse(err.message).message);
+            return setTimeout(function () {
+                process.exit();
+            }, 1000);
+        }
+        CONFIG.user = user;
+        SplashScreen.updateMessage("Logged in as: " + user.login);
+        setTimeout(function() {
+            SplashScreen.updateMessage("Fetching News Feed ...");
+            MainStream();
+        }, 1000);
+    });
+});
